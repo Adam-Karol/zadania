eabc83183bc19592b8fb7961aff21e2942f6aea4
Adam-Karol <adam.paraszkiewicz@gmail.com>
2021-09-19 11:51:31 +0200

    nowe zadanie

---
+++ CPP/dwie_wieze.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct pozycja
+{
+    int x;
+    int y;
+};
+
+void wczytaj_pozycje(pozycja& pozycja1, pozycja& pozycja2);
+
+std::vector<pozycja> oblicz_ataki(pozycja skoczek);
+
+int main()
+{
+    pozycja k1;
+    pozycja k2;
+    wczytaj_pozycje(k1, k2);
+
+    std::vector<pozycja> ataki = oblicz_ataki(k1);
+
+    for (pozycja a : ataki)
+    {
+        if (a.x == k2.x and a.y == k2.y)
+        {
+            cout << "TAK";
+            return 0;
+        }
+    }
+    cout << "NIE";
+
+    return 0;
+}
+
+std::vector<pozycja> oblicz_ataki(pozycja skoczek)
+{
+    vector<pozycja> ataki {};
+    pozycja atak = {};
+    for (int i = skoczek.x; i <= 8; i++)
+    {
+        atak = {i, skoczek.y};
+        ataki.push_back(atak);
+    }
+    for (int i = skoczek.x; i > 0; i--)
+    {
+        atak = {i, skoczek.y};
+        ataki.push_back(atak);
+    }
+    for (int i = skoczek.y; i <= 8; i++)
+    {
+        atak = {skoczek.x, i};
+        ataki.push_back(atak);
+    }
+    for (int i = skoczek.y; i > 0; i--)
+    {
+        atak = {skoczek.x, i};
+        ataki.push_back(atak);
+    }
+
+    return ataki;
+}
+
+void wczytaj_pozycje(pozycja& pozycja1, pozycja& pozycja2)
+{
+    string temp1, temp2;
+    cin >> temp1 >> temp2;
+    pozycja1.x = temp1[0]-96;
+    pozycja1.y = temp1[1]-48;
+    pozycja2.x = temp2[0]-96;
+    pozycja2.y = temp2[1]-48;
+}
