ba4bdb4a2103336eae78e3e5a659259e46e2f00d
Adam-Karol <adam.paraszkiewicz@gmail.com>
2022-01-15 15:30:02 +0100

    ulepszono zadanie

--- CPP/lista_dwukierunkowa.cpp
+++ CPP/lista_dwukierunkowa.cpp
@@ -1,138 +1,209 @@
 #include <iostream>
 using namespace std;
 
+template <typename T>
 struct elem
 {
-    int wartosc;
+    T wartosc;
     elem* nast;
     elem* poprz;
 };
 
-void dodaj_elem(elem** lista, int do_dodania)
+template <typename T>
+class ListaDwukierunkowa
 {
-    if (*lista == nullptr)
+protected:
+    elem<T>* lista;
+
+public:
+    ListaDwukierunkowa()
     {
-        *lista = new elem;
-        (*lista)->wartosc = do_dodania;
-        (*lista)->nast = nullptr;
-        (*lista)->poprz = nullptr;
+
+        this->lista = nullptr;
     }
-    else
+
+    ~ListaDwukierunkowa()
     {
-        elem *temp = *lista;
-        while (temp->nast != nullptr)
+        zwolnij_liste();
+    }
+
+    T& operator[] (int a)
+    {
+        return zwroc_wartosc_ref(a);
+    }
+
+    void dodaj_elem(const T do_dodania)
+    {
+        if (lista == nullptr)
         {
+            lista = new elem<T>;
+            lista->wartosc = do_dodania;
+            lista->nast = nullptr;
+            lista->poprz = nullptr;
+        }
+        else
+        {
+            elem<T> *temp = lista;
+            while (temp->nast != nullptr)
+            {
+                temp = temp->nast;
+            }
+            temp->nast = new elem<T>;
+            temp->nast->wartosc = do_dodania;
+            temp->nast->nast = nullptr;
+            temp->nast->poprz = temp;
+        }
+    }
+
+    void wyswietl_elem()
+    {
+        elem<T> *temp = lista;
+        cout << endl;
+        if (temp == nullptr)
+        {
+            cout << "Lista jest pusta" << endl;
+        }
+        while (temp != nullptr)
+        {
+            cout << "poprz: " <<  temp->poprz << " | ten: " << temp << " | war:" << temp->wartosc << " | nast:" << temp->nast << endl;
             temp = temp->nast;
         }
-        temp->nast = new elem;
-        temp->nast->wartosc = do_dodania;
-        temp->nast->nast = nullptr;
-        temp->nast->poprz = temp;
     }
-}
 
-void wyswietl_elem(elem* lista)
-{
-    cout << endl;
-    if (lista == nullptr)
+    T& zwroc_wartosc_ref(int index)
     {
-        cout << "Lista jest pusta" << endl;
+        if (index < 0 or index >= liczba_elem())
+        {
+            throw invalid_argument("Nieporawny index");
+        }
+        elem<T> *temp = lista;
+        int i = 0;
+        while (i != index)
+        {
+            temp = temp->nast;
+            i++;
+        }
+        return temp->wartosc;
     }
-    while (lista != nullptr)
-    {
-        cout << "poprz: " <<  lista->poprz << " | ten: " << lista << " | war:" << lista->wartosc << " | nast:" << lista->nast << endl;
-        lista = lista->nast;
-    }
-}
 
-void usun_elem(elem** lista, int do_usun)
-{
-    elem *temp = *lista;
-    if (*lista == nullptr)
-        return;
-
-    while (temp->wartosc != do_usun)
+    void usun_elem(int do_usun)
     {
-        temp = temp->nast;
-        if (temp == nullptr)
+        elem<T> *temp = lista;
+        if (lista == nullptr)
             return;
+
+        while (temp->wartosc != do_usun)
+        {
+            temp = temp->nast;
+            if (temp == nullptr)
+                return;
+        }
+
+        if (temp->poprz)
+            temp->poprz->nast = temp->nast;
+        else
+            lista = temp->nast;
+
+        if (temp->nast)
+            temp->nast->poprz = temp->poprz;
+
+        delete temp;
     }
 
-    if (temp->poprz)
-        temp->poprz->nast = temp->nast;
-    else
-        *lista = temp->nast;
-
-    if (temp->nast)
-        temp->nast->poprz = temp->poprz;
-
-    delete temp;
-}
-
-int liczba_elem(elem* lista)
-{
-    int liczba = 0;
-    while (lista != nullptr)
+    T sumowanie()
     {
-        liczba += 1;
-        lista = lista->nast;
+        elem<T> *temp = lista;
+        T suma = 0;
+
+        while (temp != nullptr)
+        {
+            suma += temp->wartosc;
+            temp = temp->nast;
+        }
+        return suma;
     }
-    return liczba;
-}
 
-int liczba_elem_o_war(elem* lista, int x)
-{
-    int liczba = 0;
-    while (lista != nullptr)
+    int liczba_elem()
     {
-        if (lista->wartosc == x)
+        elem<T> *temp = lista;
+        int liczba = 0;
+        while (temp != nullptr)
         {
             liczba += 1;
+            temp = temp->nast;
         }
-        lista = lista->nast;
+        return liczba;
     }
-    return liczba;
-}
 
-bool czy_zawiera(elem* lista, int x)
-{
-    bool czy = false;
-    while (lista != nullptr)
+    int liczba_elem_o_war(int x)
     {
-        if (lista->wartosc == x)
+        elem<T> *temp = lista;
+        int liczba = 0;
+        while (temp != nullptr)
         {
-            czy = true;
+            if (temp->wartosc == x)
+            {
+                liczba += 1;
+            }
+            temp = temp->nast;
         }
-        lista = lista->nast;
+        return liczba;
     }
-    return czy;
-}
 
-void zwolnij_liste(elem** lista)
-{
-    while (*lista != nullptr)
+    int indeks_od(T war)
     {
-        elem* nast = (*lista)->nast;
-        delete *lista;
-        *lista = nast;
+        elem<T> *temp = lista;
+        int indeks = -1;
+        int i = 0;
+        while (temp != nullptr)
+        {
+            if (temp->wartosc == war)
+            {
+                indeks = i;
+            }
+            i++;
+            temp = temp->nast;
+        }
+        if (indeks != -1)
+        {
+            return indeks;
+        }
+        throw invalid_argument("Nie ma takiego elementu w liscie");
     }
-    *lista = nullptr;
-}
+
+    bool czy_zawiera(int x)
+    {
+        elem<T> *temp = lista;
+        bool czy = false;
+        while (temp != nullptr)
+        {
+            if (temp->wartosc == x)
+            {
+                czy = true;
+            }
+            temp = temp->nast;
+        }
+        return czy;
+    }
+
+    void zwolnij_liste()
+    {
+        while (lista != nullptr)
+        {
+            elem<T>* nast = lista->nast;
+            delete lista;
+            lista = nast;
+        }
+        lista = nullptr;
+    }
+};
 
 int main()
 {
-    elem* wsk = nullptr;
-    dodaj_elem(&wsk, 12);
-    dodaj_elem(&wsk, 9);
-    dodaj_elem(&wsk, 8);
-    dodaj_elem(&wsk, 9);
-    dodaj_elem(&wsk, 9);
-    wyswietl_elem(wsk);
-    cout << liczba_elem(wsk) << endl;
-    cout << liczba_elem_o_war(wsk, 9) << endl;
-    cout << boolalpha << czy_zawiera(wsk, 12) << endl;
-    usun_elem(&wsk, 1);
-    wyswietl_elem(wsk);
-    zwolnij_liste(&wsk);
+    ListaDwukierunkowa<int> lista;
+    lista.dodaj_elem(2.1);
+    lista.dodaj_elem(3.2);
+    lista.dodaj_elem(4.3);
 
+    cout << lista.sumowanie();
 }
